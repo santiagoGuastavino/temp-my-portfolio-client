1f9f2ef2ee387a0f028b495efe38078cd90d82d0
Santiago Guastavino <san.guasta@gmail.com>
2024-05-01 01:04:03 -0300

    feat: adding desktop CSS

--- components/PageComponents/TopBar/index.tsx
+++ components/PageComponents/TopBar/index.tsx
@@ -5,6 +5,21 @@ import { Link } from "react-scroll";
 
 export default function TopBar(): JSX.Element {
   const [showMobileModalLinks, setShowMobileModalLinks] = useState<boolean>(false);
+  const [isScrolling, setIsScrolling] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const topOfThePage = window.scrollY === 0;
+
+      setIsScrolling(!topOfThePage);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleClick = (): void => {
     setShowMobileModalLinks(!showMobileModalLinks);
@@ -22,13 +37,23 @@ export default function TopBar(): JSX.Element {
 
   return (
     <>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${isScrolling ? styles.scrolling : ""}`}>
         <Icon
           icon={showMobileModalLinks ? "close" : "menu"}
           color="main"
           size={35}
+          className={styles.icon}
           onClick={() => handleClick()}
         />
+        <div className={styles.desktopLinks}>
+          {links.map((link: string, index: number) => (
+            <div key={`${link}-${index}`}>
+              <Link smooth={true} duration={500} to={link}>
+                {link}
+              </Link>
+            </div>
+          ))}
+        </div>
       </nav>
       {showMobileModalLinks ? (
         <section className={styles.mobileSection}>
