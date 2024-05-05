c4a462582f0bd99afd7d0552e0c00132892d8313
Santiago Guastavino <san.guasta@gmail.com>
2024-05-04 21:03:53 -0300

    feat: improved experience, finished base section web layout

--- components/PageComponents/Experience/index.tsx
+++ components/PageComponents/Experience/index.tsx
@@ -10,15 +10,20 @@ export default function Experience(): JSX.Element {
       {experiences.map((experience: IExperience, index: number) => (
         <article className={styles.article} key={`${experience.company}-${index}`}>
           <h3 className={styles.title}>{experience.title}</h3>
-          <h3 className={styles.company}>{experience.company}</h3>
-          <div className={styles.fromUntil}>
-            <span>{experience.from}</span>
-            <span>-</span>
-            <span>{experience.until}</span>
+          <div className={styles.subTitle}>
+            <h3 className={styles.company}>{experience.company}</h3>
+            <div className={styles.fromUntil}>
+              <span>{experience.from}</span>
+              <span>-</span>
+              <span>{experience.until}</span>
+            </div>
           </div>
           <div className={styles.tech}>
             {experience.tech.map((tech: string, index: number) => (
-              <span key={`${tech}-${index}`}>{tech}</span>
+              <>
+                <span key={`${tech}-${index}`}>{tech}</span>
+                {index !== experience.tech.length - 1 && <span>|</span>}
+              </>
             ))}
           </div>
         </article>
@@ -29,50 +34,45 @@ export default function Experience(): JSX.Element {
 
 const experiences: IExperience[] = [
   {
-    company: "Shifta",
-    title: "Software Engineer",
-    from: "May 2023",
+    company: "Digital House",
+    title: "Front-end web developer",
+    from: "May 2024",
     until: "Present",
-    tech: [
-      "Typescript",
-      "Node.js",
-      "Next.js",
-      "React.js",
-      "Nest.js",
-      "Zustand",
-      "Clean Architecture",
-    ],
+    tech: ["Typescript", "Next.js"],
+  },
+  {
+    company: "Shifta",
+    title: "Full stack web developer",
+    from: "May 2023",
+    until: "May 2024",
+    tech: ["Typescript", "Next.js", "Nest.js"],
   },
   {
     company: "Limboteams",
     title: "Back-end web developer",
-    from: "August 2022",
+    from: "Aug 2022",
     until: "May 2023",
-    tech: [
-      "Typescript",
-      "Node.js",
-      "Nest.js",
-      "MongoDB",
-      "OOP",
-      "Multitier Architecture",
-      "TDD",
-      "Web Sockets",
-      "Dependency Injection",
-      "AWS",
-    ],
+    tech: ["Typescript", "Nest.js", "MongoDB"],
+  },
+  {
+    company: "Punto de compra",
+    title: "Front-end web developer",
+    from: "Aug 2022",
+    until: "Dec 2022",
+    tech: ["Javascript", "React.js", "Vue.js"],
   },
   {
     company: "Pixel Ecom",
     title: "Full stack web developer",
-    from: "September 2021",
-    until: "August 2022",
-    tech: ["Javascript", "Node.js", "Express.js", "React.js", "MySQL", "PHP"],
+    from: "Sep 2021",
+    until: "Aug 2022",
+    tech: ["Javascript", "Express.js", "React.js"],
   },
   {
     company: "Game central",
     title: "Full stack web developer",
-    from: "March 2021",
-    until: "September 2021",
-    tech: ["Javascript", "Node.js", "Express.js", "MVC", "React.js", "Heroku"],
+    from: "Mar 2021",
+    until: "Sep 2021",
+    tech: ["Javascript", "Express.js", "MVC"],
   },
 ];
